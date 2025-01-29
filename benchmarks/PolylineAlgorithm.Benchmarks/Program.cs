5e56159288cde61e9be8ab21bed9e3a228173f70
Petr Sramek <petr.sramek@outlook.cz>
2025-01-29 20:32:04 +0100

    fix

--- benchmarks/PolylineAlgorithm.Benchmarks/Program.cs
+++ benchmarks/PolylineAlgorithm.Benchmarks/Program.cs
@@ -5,12 +5,20 @@
 
 namespace PolylineAlgorithm.Benchmarks;
 
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 
 internal class Program {
     static void Main(string[] args) {
+        var config = DefaultConfig.Instance
+            .AddJob(Job.Default.WithRuntime(CoreRuntime.Core70))
+            .AddJob(Job.Default.WithRuntime(CoreRuntime.Core80))
+            .AddJob(Job.Default.WithRuntime(CoreRuntime.Core90));
+
         BenchmarkSwitcher
             .FromAssembly(typeof(Program).Assembly)
-            .Run(args);
+            .Run(args, config);
     }
 }
