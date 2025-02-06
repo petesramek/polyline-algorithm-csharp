e26f7d5a97bb551cee5761afd54606291de1b221
Petr Sramek <petr.sramek@outlook.cz>
2025-02-06 22:14:49 +0100

    zcds

--- benchmarks/PolylineAlgorithm.Benchmarks/Program.cs
+++ benchmarks/PolylineAlgorithm.Benchmarks/Program.cs
@@ -6,27 +6,12 @@
 namespace PolylineAlgorithm.Benchmarks;
 
 using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Environments;
-using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 
 internal class Program {
     static void Main(string[] args) {
-        var config = DefaultConfig.Instance
-            //.AddJob(Job.Default.WithRuntime(CoreRuntime.Core20))
-            //.AddJob(Job.Default.WithRuntime(CoreRuntime.Core21))
-            //.AddJob(Job.Default.WithRuntime(CoreRuntime.Core22))
-            //.AddJob(Job.Default.WithRuntime(CoreRuntime.Core30))
-            //.AddJob(Job.Default.WithRuntime(CoreRuntime.Core31))
-            .AddJob(Job.Default.WithRuntime(CoreRuntime.Core50))
-            .AddJob(Job.Default.WithRuntime(CoreRuntime.Core60))
-            .AddJob(Job.Default.WithRuntime(CoreRuntime.Core70))
-            .AddJob(Job.Default.WithRuntime(CoreRuntime.Core80))
-            .AddJob(Job.Default.WithRuntime(CoreRuntime.Core90))
-            ;
-
         BenchmarkSwitcher
             .FromAssembly(typeof(Program).Assembly)
-            .Run(args, config);
+            .Run(args, DefaultConfig.Instance);
     }
 }
