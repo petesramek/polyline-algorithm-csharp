fbd13c04c6475fdcf4da7c97ad04524c863735f8
Petr Sramek <petr.sramek@outlook.cz>
2025-02-09 12:34:58 +0100

    imporvements

--- tests/PolylineAlgorithm.Tests/PolylineTest.cs
+++ tests/PolylineAlgorithm.Tests/PolylineTest.cs
@@ -14,17 +14,17 @@ using System;
 /// </summary>
 [TestClass]
 public class PolylineTest {
-    public static IEnumerable<object[]> MemoryConstructorParamaters => [
+    public static IEnumerable<object[]> MemoryParamaters => [
         [Defaults.Polyline.Empty.AsMemory()],
         [Defaults.Polyline.Valid.AsMemory()]
     ];
 
-    public static IEnumerable<object[]> StringConstructorParamaters => [
+    public static IEnumerable<object[]> StringParamaters => [
         [Defaults.Polyline.Empty],
         [Defaults.Polyline.Valid]
     ];
 
-    public static IEnumerable<object[]> CharArrayConstructorParamaters => [
+    public static IEnumerable<object[]> CharArrayParamaters => [
         [Defaults.Polyline.Empty.ToCharArray()],
         [Defaults.Polyline.Valid.ToCharArray()]
     ];
@@ -47,7 +47,7 @@ public class PolylineTest {
     }
 
     [TestMethod]
-    [DynamicData(nameof(StringConstructorParamaters))]
+    [DynamicData(nameof(StringParamaters))]
     public void Constructor_Empty_String_Ok(string value) {
         // Arrange
         bool isEmpty = value.Length == 0;
@@ -65,7 +65,7 @@ public class PolylineTest {
 
 
     [TestMethod]
-    [DynamicData(nameof(CharArrayConstructorParamaters))]
+    [DynamicData(nameof(CharArrayParamaters))]
     public void Constructor_Empty_CharArray_Ok(char[] value) {
         // Arrange
         bool isEmpty = value.Length == 0;
@@ -82,7 +82,7 @@ public class PolylineTest {
     }
 
     [TestMethod]
-    [DynamicData(nameof(MemoryConstructorParamaters))]
+    [DynamicData(nameof(MemoryParamaters))]
     public void Constructor_Memory_Paramater_Ok(ReadOnlyMemory<char> value) {
         // Arrange
         bool isEmpty = value.IsEmpty;
@@ -99,130 +99,81 @@ public class PolylineTest {
     }
 
     [TestMethod]
-    public void Constructor_Valid_String_Ok() {
+    [DynamicData(nameof(StringParamaters))]
+    public void FromString_Equals_New(string value) {
         // Arrange
-        string value = Defaults.Polyline.Valid;
-        int expectedLength = value.Length;
-        ReadOnlySpan<char> expectedSpan = value.AsSpan();
-
-        // Act
         Polyline polyline = new(value);
 
+        // Act
+        Polyline result = Polyline.FromString(in value);
+
         // Assert
-        Assert.IsFalse(polyline.IsEmpty);
-        Assert.IsTrue(expectedSpan.SequenceEqual(polyline.Span));
-        Assert.AreEqual(expectedLength, polyline.Length);
+        Assert.AreEqual(polyline, result);
     }
 
     [TestMethod]
-    public void Constructor_Valid_CharArray_Ok() {
+    [DynamicData(nameof(CharArrayParamaters))]
+    public void FromCharArray_Equals_New(char[] value) {
         // Arrange
-        char[] value = Defaults.Polyline.Valid.ToCharArray();
-        int expectedLength = value.Length;
-        ReadOnlySpan<char> expectedSpan = value.AsSpan();
-
-        // Act
         Polyline polyline = new(value);
 
+        // Act
+        Polyline result = Polyline.FromCharArray(in value);
+
         // Assert
-        Assert.IsFalse(polyline.IsEmpty);
-        Assert.IsTrue(expectedSpan.SequenceEqual(polyline.Span));
-        Assert.AreEqual(expectedLength, polyline.Length);
+        Assert.AreEqual(polyline, result);
     }
 
-
     [TestMethod]
-    public void Constructor_Valid_Memory_Ok() {
+    [DynamicData(nameof(MemoryParamaters))]
+    public void FromMemory_Equals_New(ReadOnlyMemory<char> value) {
         // Arrange
-        ReadOnlyMemory<char> value = Defaults.Polyline.Valid.AsMemory();
-        int expectedLength = value.Length;
-        ReadOnlySpan<char> expectedSpan = value.Span;
-
-        // Act
         Polyline polyline = new(value);
 
-        // Assert
-        Assert.IsFalse(polyline.IsEmpty);
-        Assert.IsTrue(expectedSpan.SequenceEqual(polyline.Span));
-        Assert.AreEqual(expectedLength, polyline.Length);
-    }
-
-    [TestMethod]
-    public void FromString_Equals_New() {
-        // Arrange
-        string value = Defaults.Polyline.Valid;
-        Polyline expectedPolyline = new(value);
-
         // Act
-        Polyline polyline = Polyline.FromString(in value);
+        Polyline result = Polyline.FromMemory(in value);
 
         // Assert
-        Assert.AreEqual(expectedPolyline, polyline);
-    }
-
-    [TestMethod]
-    public void FromCharArray_Equals_New() {
-        // Arrange
-        char[] value = Defaults.Polyline.Valid.ToCharArray();
-        Polyline expectedPolyline = new(value);
-
-        // Act
-        Polyline polyline = Polyline.FromCharArray(in value);
-
-        // Assert
-        Assert.AreEqual(expectedPolyline, polyline);
-    }
-
-    [TestMethod]
-    public void FromMemory_Equals_New() {
-        // Arrange
-        ReadOnlyMemory<char> value = Defaults.Polyline.Valid.AsMemory();
-        Polyline expectedPolyline = new(value);
-
-        // Act
-        Polyline polyline = Polyline.FromMemory(in value);
-
-        // Assert
-        Assert.AreEqual(expectedPolyline, polyline);
+        Assert.AreEqual(polyline, result);
     }
 
 
     [TestMethod]
-    public void ToString_Equals_Constructor_Parameter() {
+    [DynamicData(nameof(StringParamaters))]
+    public void ToString_Equals_Constructor_Parameter(string value) {
         // Arrange
-        Polyline polyline = new(Defaults.Polyline.Valid);
-        string expectedResult = Defaults.Polyline.Valid;
+        Polyline polyline = new(value);
 
         // Act
         string result = polyline.ToString();
 
         // Assert
-        Assert.AreEqual(expectedResult, result);
+        Assert.AreEqual(value, result);
     }
 
     [TestMethod]
-    public void ToCharArray_Equals_Constructor_Parameter() {
+    [DynamicData(nameof(CharArrayParamaters))]
+    public void ToCharArray_Equals_Constructor_Parameter(char[] value) {
         // Arrange
-        Polyline polyline = new(Defaults.Polyline.Valid);
-        char[] expectedResult = Defaults.Polyline.Valid.ToCharArray();
+        Polyline polyline = new(value);
 
         // Act
         char[] result = polyline.ToCharArray();
 
         // Assert
-        CollectionAssert.AreEqual(expectedResult, result);
+        CollectionAssert.AreEqual(value, result);
     }
 
     [TestMethod]
-    public void AsMemory_Equals_Constructor_Parameter() {
+    [DynamicData(nameof(MemoryParamaters))]
+    public void AsMemory_Equals_Constructor_Parameter(ReadOnlyMemory<char> value) {
         // Arrange
-        Polyline polyline = new(Defaults.Polyline.Valid);
-        ReadOnlyMemory<char> expectedResult = Defaults.Polyline.Valid.AsMemory();
+        Polyline polyline = new(value);
 
         // Act
         ReadOnlyMemory<char> result = polyline.AsMemory();
 
         // Assert
-        Assert.AreEqual(expectedResult, result);
+        Assert.AreEqual(value, result);
     }
 }
