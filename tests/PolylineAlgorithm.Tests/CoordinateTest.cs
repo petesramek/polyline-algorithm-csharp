21b79cdfa25184399eaa6d2ff1ff0bf06d7a8841
Petr Sramek <petr.sramek@outlook.cz>
2025-02-09 15:25:33 +0100

    added initial coordinate tests

---
+++ tests/PolylineAlgorithm.Tests/CoordinateTest.cs
@@ -0,0 +1,54 @@
+﻿//  
+// Copyright (c) Pete Sramek. All rights reserved.  
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.  
+//
+
+namespace PolylineAlgorithm.Tests;
+
+using Newtonsoft.Json.Linq;
+using PolylineAlgorithm.Tests.Internal;
+using System;
+
+/// <summary>
+/// Tests <see cref="Polyline"/> type.
+/// </summary>
+[TestClass]
+public class CoordinateTest {
+    public static IEnumerable<object[]> ValidParamaters => [
+        [ 0d, 0d, true, true ],
+        [ 90, 180, true, false ],
+        [ -90, -180, true, false  ],
+        [ 90, -180, true, false  ],
+        [ -90, 180, true, false  ],
+    ];
+
+    [TestMethod]
+    public void Constructor_Parameterless_Ok() {
+        // Arrange
+        double latitude = 0d;
+        double longitude = 0d;
+
+        // Act
+        Coordinate result = new();
+
+        // Assert
+        Assert.IsTrue(result.IsValid);
+        Assert.IsTrue(result.IsDefault);
+        Assert.AreEqual(latitude, result.Latitude);
+        Assert.AreEqual(longitude, result.Longitude);
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(ValidParamaters))]
+    public void Constructor_Double_Double_Paramaters_Ok(double latitude, double longitude, bool valid, bool @default) {
+        // Arrange
+        // Act
+        Coordinate result = new(latitude, longitude);
+
+        // Assert
+        Assert.AreEqual(valid, result.IsValid);
+        Assert.AreEqual(@default, result.IsDefault);
+        Assert.AreEqual(latitude, result.Latitude);
+        Assert.AreEqual(longitude, result.Longitude);
+    }
+}
