1cff8c20d0479b9c8ca083e7748f033a38412689
Petr Sramek <petr.sramek@outlook.cz>
2025-02-14 11:30:27 +0100

    remoevd unused code

--- benchmarks/PolylineAlgorithm.Benchmarks/Program.cs
+++ benchmarks/PolylineAlgorithm.Benchmarks/Program.cs
@@ -6,15 +6,10 @@
 namespace PolylineAlgorithm.Benchmarks;
 
 using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Running;
 
 internal class Program {
     static void Main(string[] args) {
-        var config = DefaultConfig.Instance
-            .AddDiagnoser(new MemoryDiagnoser(new MemoryDiagnoserConfig(true)))
-            .WithOptions(ConfigOptions.JoinSummary);
-
         BenchmarkSwitcher
             .FromAssembly(typeof(Program).Assembly)
             .Run(args, DefaultConfig.Instance);
