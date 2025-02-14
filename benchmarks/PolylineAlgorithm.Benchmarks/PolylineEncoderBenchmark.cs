d19063d6d5dc873762c6cf4e27fd789f8b5603d8
Petr Sramek <petr.sramek@outlook.cz>
2025-02-14 10:26:21 +0100

    added columns

--- benchmarks/PolylineAlgorithm.Benchmarks/PolylineEncoderBenchmark.cs
+++ benchmarks/PolylineAlgorithm.Benchmarks/PolylineEncoderBenchmark.cs
@@ -13,6 +13,8 @@ using System.Collections.Generic;
 
 [Orderer(SummaryOrderPolicy.Declared)]
 [BenchmarkCategory(BenchmarkCategory.PublicApi)]
+[CategoriesColumn]
+[RankColumn]
 public class PolylineEncoderBenchmark {
     private static readonly Random R = new();
     public static List<Coordinate> GetList() {
