b7f2986affcbbac42b7f11d52d4184e686bda590
Petr Sramek <petr.sramek@outlook.cz>
2025-02-14 16:56:32 +0100

    updated polyline to 100 coordinates

--- benchmarks/PolylineAlgorithm.Benchmarks/PolylineDecoderBenchmark.cs
+++ benchmarks/PolylineAlgorithm.Benchmarks/PolylineDecoderBenchmark.cs
@@ -17,7 +17,7 @@ using PolylineAlgorithm.Benchmarks.Internal;
 [RankColumn]
 public class PolylineDecoderBenchmark {
     private readonly Consumer _consumer = new();
-    public static Polyline Polyline { get; } = (Polyline)"}adrJh}}cVazlw@uykyNhaqeE`vfzG_~kY}~`eTsr{~Cwn~aOty_g@thapJvvoqKxt{sStfahDmtvmIfmiqBhjq|HujpgComs{Z}dhdKcidPymnvBqmquE~qrfI`x{lPf|ftGn~}d_@q}saAurjmu@bwr_DxrfaK~{rO~bidPwfduXwlioFlpum@twvfFpmi~VzxcsOqyejYhh|i@pbnr[twvfF_ueUujvbSa_d~ZkcnjZla~f[pmquEebxo[j}nr@xnn|H{gyiKbh{yH`oenn@y{mpIrbd~EmipgH}fuov@hjqtTp|flAttvkFrym_d@|eyCwn~aOfvdNmeawM??{yxdUcidPca{}D_atqGenzcAlra{@trgWhn{aZ??tluqOgu~sH";
+    public static Polyline Polyline { get; } = (Polyline)"|koyD_tmn[|sfDltuii@qjarFkvifi@uepdF~bwdSb{cfSf|y{Nhc}Gfp}~OabanXo{p_t@kfx[rb`yDaiwkAncxm]beulDitjhYqsloAiigeM`uzPhtkxDqiaT`pplw@jzfvXx~i|B_dxuC_sk{[uqktKstyl@pra`BuwasUmvjr@rvs{j@x`dcM}tqbe@mkbiJr_zbFlpc}K~rt{BetlmQw`wnQj|ckKobv{Icbw~Bf}p|b@}mcnDa_usb@zkc|G`pgg\\\\ifuhBhbe~Z~ul`NilxbHunrxZvo}qJ`}efQvfaaAty{yDx_kE{vf_Hwns`r@ucq`NzfziVxznnEz`vaD|yzrMkkjf`@{sewCxrv`i@pbrg@cpfeUgulnBsmapKhdbnHsrusCqt}sHvajfl@mygsRl{aoF`|{eQn`hFjhi_Esj_tk@lsfk@}rpgAtggHtgzQprpjBphmn]wacxWdpiyDdtxdAeqtrBjkjAscknk@_egt@s}qiBxp}eUhypv^onrxSnyndPvazxViswuX_efmTvy`gApigfTb`|uB_bacSnxjbHvihiRmrxsGsy{qBfihyIcrexM{{cle@jcjpDzbx`\\\\u~jzEyjseJcw`pEw`rlUpew|JlqqcKj}~uFlmk`GmjudUqj}~Jz|_rAb~bgH|lstUfh|gj@vwygCmlyhWwhbmCc}uzTwvklKam|dDasi{KrmenSpi_Bzkznb@xsdaKcvzsq@~~f{Fz_}}Pq}nrA{qe_\\\\lmw}BxvzwSaqbiBkqnyLumkhF`nnic@mntzEsz_oc@tikmIffhob@rnauKccmp]jmdkDbcorUqkerQ~gby\\\\lfytIml`sSbiyyC_njae@gvrnIjsahTg|}xGuw~pWywyyCjgag|@dphjZgmh~]k}zxYc~ciNdmzwHp}rhB`rkhJrafaIghfvVxdzp`@ffctJetj~b@bz|rFd_oaGqslzMo{djUlwtaSrowsb@czmtQenbnWlkqjS~hhwEwxqc@vnzyP";
 
     [Benchmark]
     public void PolylineDecoder_Decode() {
