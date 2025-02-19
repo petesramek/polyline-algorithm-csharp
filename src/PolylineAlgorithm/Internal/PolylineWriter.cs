f28a42a9a1d21508bdea3c231465ce13aeb7df41
Petr Sramek <petr.sramek@outlook.cz>
2025-02-19 17:33:50 +0000

    applied file header rule

--- src/PolylineAlgorithm/Internal/PolylineWriter.cs
+++ src/PolylineAlgorithm/Internal/PolylineWriter.cs
@@ -1,4 +1,9 @@
-﻿namespace PolylineAlgorithm.Internal;
+﻿//
+// Copyright © Pete Sramek. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//
+
+namespace PolylineAlgorithm.Internal;
 
 using System;
 using System.Runtime.CompilerServices;
