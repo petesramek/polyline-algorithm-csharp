64fa199ab6b1093e7d20b5a624388a3710312702
Pete Sramek <me@petesramek.com>
2025-07-05 19:36:33 +0200

    added ids to logger messages

--- src/PolylineAlgorithm.Abstraction/Internal/Logging.cs
+++ src/PolylineAlgorithm.Abstraction/Internal/Logging.cs
@@ -3,21 +3,21 @@
 using Microsoft.Extensions.Logging;
 
 internal static partial class Logging {
-    [LoggerMessage(0, LogLevel.Error, "Argument {argumentName} cannot be null.")]
+    [LoggerMessage(1, LogLevel.Error, "Argument {argumentName} cannot be null.")]
     internal static partial void LogNullArgumentError(this ILogger logger, string argumentName);
 
-    [LoggerMessage(0, LogLevel.Error, "Argument {argumentName} cannot be empty.")]
+    [LoggerMessage(2, LogLevel.Error, "Argument {argumentName} cannot be empty.")]
     internal static partial void LogEmptyArgumentError(this ILogger logger, string argumentName);
 
-    [LoggerMessage(0, LogLevel.Error, "Internal buffer has {bufferLength} length. At position {position} is required additonal {requiredSpace} length.")]
+    [LoggerMessage(3, LogLevel.Error, "Internal buffer has {bufferLength} length. At position {position} is required additonal {requiredSpace} length.")]
     internal static partial void LogInternalBufferOverflowError(this ILogger logger, int position, int bufferLength, int requiredSpace);
     
-    [LoggerMessage(0, LogLevel.Error, "Cannot write to internal buffer at position {position}.")]
+    [LoggerMessage(4, LogLevel.Error, "Cannot write to internal buffer at position {position}.")]
     internal static partial void LogCannotWriteValueToBufferError(this ILogger logger, int position);
 
-    [LoggerMessage(0, LogLevel.Error, "Argument {argumentName} is too short. Minimal length is {minimumLength}. Actual length is {actualLength}.")]
+    [LoggerMessage(5, LogLevel.Error, "Argument {argumentName} is too short. Minimal length is {minimumLength}. Actual length is {actualLength}.")]
     internal static partial void LogPolylineCannotBeShorterThanError(this ILogger logger, string argumentName, int actualLength, int minimumLength);
 
-    [LoggerMessage(0, LogLevel.Warning, "Requested buffer size of {requestedBufferLength} exceeds maximum allowed buffer length of {maxBufferLength}.")]
+    [LoggerMessage(6, LogLevel.Warning, "Requested buffer size of {requestedBufferLength} exceeds maximum allowed buffer length of {maxBufferLength}.")]
     internal static partial void LogRequestedBufferSizeExceedsMaxBufferLength(this ILogger logger, int requestedBufferLength, int maxBufferLength);
 }
