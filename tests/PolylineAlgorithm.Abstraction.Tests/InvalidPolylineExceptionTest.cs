e959c0399abc9056deb5a0eef48cf2f8dab43a4d
Pete Sramek <me@petesramek.com>
2025-07-05 19:39:46 +0200

    updated namespaces

--- tests/PolylineAlgorithm.Abstraction.Tests/InvalidPolylineExceptionTest.cs
+++ tests/PolylineAlgorithm.Abstraction.Tests/InvalidPolylineExceptionTest.cs
@@ -3,7 +3,7 @@
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 //
 
-namespace PolylineAlgorithm.Tests;
+namespace PolylineAlgorithm.Abstraction.Tests;
 
 using PolylineAlgorithm.Abstraction;
 
--- tests/PolylineAlgorithm.Abstraction.Tests/Properties/Category.cs
+++ tests/PolylineAlgorithm.Abstraction.Tests/Properties/Category.cs
@@ -3,7 +3,7 @@
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 //
 
-namespace PolylineAlgorithm.Tests.Properties;
+namespace PolylineAlgorithm.Abstraction.Tests.Properties;
 
 /// <summary>
 /// Provides constants for test categories.
