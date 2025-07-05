8bd18c1e11e1c8c5d1c7c04a302d6bda9125a7d5
Pete Sramek <me@petesramek.com>
2025-07-05 14:30:03 +0200

    removed comments

--- tests/PolylineAlgorithm.Abstraction.Tests/FakeLoggerFactory.cs
+++ tests/PolylineAlgorithm.Abstraction.Tests/FakeLoggerFactory.cs
@@ -27,14 +27,11 @@ internal class FakeLoggerFactory : ILoggerFactory {
                
             }
 
-            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-            // TODO: set large fields to null
             _isDisposed = true;
         }
     }
 
     public void Dispose() {
-        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
         Dispose(disposing: true);
         GC.SuppressFinalize(this);
     }
