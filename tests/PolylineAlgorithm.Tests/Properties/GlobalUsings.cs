8413bc7f2d21e171a025151aa288b3fdd2107027
Pete Sramek <me@petesramek.com>
2025-07-08 21:58:36 +0200

    remove all

--- tests/PolylineAlgorithm.Tests/Properties/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-//
-// Copyright © Pete Sramek. All rights reserved.
-// Licensed under the MIT License. See LICENSE file in the project root for full license information.
-//
-
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
