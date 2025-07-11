383ef8f46e0617b0b9cb1c7147518daa6e495263
Pete Sramek <me@petesramek.com>
2025-07-11 15:09:29 +0200

    added debugger display attr

--- src/PolylineAlgorithm.Abstraction/PolylineEncodingOptions.cs
+++ src/PolylineAlgorithm.Abstraction/PolylineEncodingOptions.cs
@@ -7,6 +7,7 @@ namespace PolylineAlgorithm.Abstraction;
 
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
+using System.Diagnostics;
 
 /// <summary>
 /// Options for configuring polyline encoding.
@@ -14,6 +15,7 @@ using Microsoft.Extensions.Logging.Abstractions;
 /// <remarks>
 /// This class allows you to set options such as buffer size and logger factory for encoding operations.
 /// </remarks>
+[DebuggerDisplay("BufferSize: {BufferSize}, MaxBufferLength: {MaxBufferLength}, LoggerFactoryType: {LoggerFactory.GetType().Name}")]
 public sealed class PolylineEncodingOptions {
     /// <summary>
     /// Gets the maximum buffer size for encoding operations.
@@ -21,15 +23,7 @@ public sealed class PolylineEncodingOptions {
     /// <remarks>
     /// The default buffer size is 64,000 bytes (64 KB). This can be adjusted based on the expected size of the polyline data.
     /// </remarks>
-    public int BufferSize { get; internal set; } = 64_000;
-
-    /// <summary>
-    /// Gets the maximum length of the encoded polyline string.
-    /// </summary>
-    /// <remarks>
-    /// The maximum length is calculated based on the buffer size divided by the size of a character.
-    /// </remarks>
-    public int MaxLength => BufferSize / sizeof(char);
+    public int BufferSizeInBytes { get; internal set; } = 64_000;
 
     /// <summary>
     /// Gets or sets the precision for encoding coordinates.
@@ -38,4 +32,13 @@ public sealed class PolylineEncodingOptions {
     /// The default logger factory is <see cref="NullLoggerFactory"/>, which does not log any messages.
     /// </remarks>
     public ILoggerFactory LoggerFactory { get; internal set; } = NullLoggerFactory.Instance;
+
+
+    /// <summary>
+    /// Gets the maximum length of the encoded polyline string.
+    /// </summary>
+    /// <remarks>
+    /// The maximum length is calculated based on the buffer size divided by the size of a character.
+    /// </remarks>
+    internal int MaxBufferLength => BufferSizeInBytes / sizeof(char);
 }
