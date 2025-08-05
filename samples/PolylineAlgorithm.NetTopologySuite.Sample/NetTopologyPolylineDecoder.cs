c6aff1dcd75cdc26dc9e03fa25a772581d39a1c4
Pete Sramek <me@petesramek.com>
2025-08-05 10:31:28 +0200

    removed program and made the project library

--- samples/PolylineAlgorithm.NetTopologySuite.Sample/NetTopologyPolylineDecoder.cs
+++ samples/PolylineAlgorithm.NetTopologySuite.Sample/NetTopologyPolylineDecoder.cs
@@ -9,12 +9,27 @@ using global::NetTopologySuite.Geometries;
 using PolylineAlgorithm.Abstraction;
 using System;
 
-internal sealed class NetTopologyPolylineDecoder : AbstractPolylineDecoder<string, Point> {
+/// <summary>
+/// Represents a polyline decoder that converts encoded polyline strings into a collection of geographic coordinates using NetTopologySuite.
+/// </summary>
+public sealed class NetTopologyPolylineDecoder : AbstractPolylineDecoder<string, Point> {
     protected override Point CreateCoordinate(double latitude, double longitude) {
         return new Point(latitude, longitude);
     }
 
-    protected override ReadOnlyMemory<char> GetReadOnlyMemory(string? polyline) {
+    /// <summary>
+    /// Converts the provided polyline string into a read-only memory of characters.
+    /// </summary>
+    /// <param name="polyline">
+    /// The encoded polyline string to be converted into a read-only memory of characters.
+    /// </param>
+    /// <returns>
+    /// A <see cref="ReadOnlyMemory{T}"/> containing the characters of the polyline string.
+    /// </returns>
+    /// <exception cref="ArgumentException">
+    /// Thrown when the provided polyline string is null, empty, or consists only of whitespace characters.
+    /// </exception>
+    protected override ReadOnlyMemory<char> GetReadOnlyMemory(string polyline) {
         if (string.IsNullOrWhiteSpace(polyline)) {
             throw new ArgumentException("Value cannot be null, empty or whitespace.", nameof(polyline));
         }
--- samples/PolylineAlgorithm.NetTopologySuite.Sample/NetTopologyPolylineEncoder.cs
+++ samples/PolylineAlgorithm.NetTopologySuite.Sample/NetTopologyPolylineEncoder.cs
@@ -8,8 +8,19 @@ namespace PolylineAlgorithm.NetTopologySuite.Sample;
 using global::NetTopologySuite.Geometries;
 using PolylineAlgorithm.Abstraction;
 
-internal sealed class NetTopologyPolylineEncoder : AbstractPolylineEncoder<Point, string> {
-
+/// <summary>
+/// Encodes a collection of geographic coordinates into an encoded polyline string using NetTopologySuite's Point type.
+/// </summary>
+public sealed class NetTopologyPolylineEncoder : AbstractPolylineEncoder<Point, string> {
+    /// <summary>
+    /// Creates a string representation of the provided polyline.
+    /// </summary>
+    /// <param name="polyline">
+    /// The polyline to encode as a string.
+    /// </param>
+    /// <returns>
+    /// An encoded polyline string representation of the provided polyline.
+    /// </returns>
     protected override string CreatePolyline(ReadOnlyMemory<char> polyline) {
         if (polyline.IsEmpty) {
             return string.Empty;
@@ -18,11 +29,33 @@ internal sealed class NetTopologyPolylineEncoder : AbstractPolylineEncoder<Point
         return polyline.ToString();
     }
 
-    protected override double GetLatitude(Point? current) {
-        return current?.X ?? 0d;
+    /// <summary>
+    /// Extracts the latitude value from the specified coordinate.
+    /// </summary>
+    /// <param name="current">
+    /// The coordinate from which to extract the latitude value. This should be a not null <see cref="Point"/> instance.
+    /// </param>
+    /// <returns>
+    /// The latitude value as a <see cref="double"/>.
+    /// </returns>
+    protected override double GetLatitude(Point current) {
+        // Validate parameter
+
+        return current.X;
     }
 
-    protected override double GetLongitude(Point? current) {
-        return current?.Y ?? 0d;
+    /// <summary>
+    /// Extracts the longitude value from the specified coordinate.
+    /// </summary>
+    /// <param name="current">
+    /// The coordinate from which to extract the longitude value. This should be a not null <see cref="Point"/> instance.
+    /// </param>
+    /// <returns>
+    /// The longitude value as a <see cref="double"/>.
+    /// </returns>
+    protected override double GetLongitude(Point current) {
+        // Validate parameter
+
+        return current.Y;
     }
 }
