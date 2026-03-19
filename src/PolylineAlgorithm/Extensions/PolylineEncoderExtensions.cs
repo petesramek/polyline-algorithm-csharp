169c8f4833df3ffabb98a6f802ceff2d933d31ac
Pete Sramek <me@petesramek.com>
2026-03-19 16:12:43 +0100

    removed header test

--- src/PolylineAlgorithm/Extensions/PolylineEncoderExtensions.cs
+++ src/PolylineAlgorithm/Extensions/PolylineEncoderExtensions.cs
@@ -1,7 +1,4 @@
-﻿//
-// Copyright © Pete Sramek. All rights reserved.
-// Licensed under the MIT License. See LICENSE file in the project root for full license information.
-//
+﻿
 
 namespace PolylineAlgorithm.Extensions;
 
